d98d96840e27172a6cb1f435de7d56b40feb594e
Margie97 <wariaramargie97@gmail.com>
2022-07-06 00:29:21 -0700

    Hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  * print_alphabet -> prints the lowercase alphabets
  */
 
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	int j;
 
-	for (j = 'a'; j <= 'z'; j++)
+	for (j = 'a'; j <= 'z'; j++);
 	{
 		_putchar (j);
 	}
