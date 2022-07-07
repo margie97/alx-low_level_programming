85af15eb96f86cf695b5a1fa85a619e052d48827
Margie97 <wariaramargie97@gmail.com>
2022-07-07 11:52:39 -0700

    Hot fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include "main.h"
 
@@ -13,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || ((year % 400 == 0) && (year % 100 != 0)))
+	if (year % 400 == 0 || ((year % 4 == 0) && (year % 100 != 0)))
 	{
 		if (month > 3 && day >= 60)
 		{
