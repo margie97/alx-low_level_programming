fe1c80da42562c0ecaf3044bcbd1810ab603e5cd
margie97 <wariaramargie97@gmail.com>
2022-07-14 12:06:19 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,27 +5,27 @@
  * Return: the pointer to dest.
  */
 
+
 char *cap_string(char *s)
 {
-	int n = 0;
-	int i;
-	int cap_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count = 0, i;
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + n) >= 97 && *(s + n) <= 122)
-		*(s + n) = *(s + n) - 32;
-	n++;
-	while (*(s + n) != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + n) == cap_words[i])
+			if (*(s + count) == separators[i])
 			{
-				if ((*(s + (n + 1)) >= 97) && (*(s + (n + 1)) <= 122))
-					*(s + (n + 1)) = *(s + (n + 1)) - 32;																				break;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
-		n++;
-
+		count++;
 	}
 	return (s);
 }
